302889a6c78dca24b14c2d706c70733c06eaf1a8
Ecem <ecemnazgorusuk@gmail.com>
2024-08-11 22:09:07 +0300

    cypress cloud's projectId added

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "1vizwi",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
