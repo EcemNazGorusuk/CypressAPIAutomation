6d919d3bd82e588c2df0908b451b30df5400ef7f
Ecem <ecemnazgorusuk@gmail.com>
2024-08-11 18:23:09 +0300

    userTests updated

--- cypress/e2e/tests/userTests.cy.js
+++ cypress/e2e/tests/userTests.cy.js
@@ -1,11 +1,68 @@
-/// <reference types="cypress" />
-
-describe("User", () => {
-  beforeEach(() => {
-    cy.visit("");
+describe("User Tests", () => {
+  it("User Create", () => {
+    //This test sends a POST request to the Petstore API and checks
+    //if the response status is 200, indicating success.
+    const postData = {
+      id: 54321,
+      username: "ecem",
+      firstName: "ecem",
+      lastName: "naz",
+      email: "ecemnazgorusuk@gmail.com",
+      password: "123",
+      phone: "05053456765",
+      userStatus: 0,
+    };
+    cy.request({
+      method: "POST",
+      url: "https://petstore.swagger.io/v2/user",
+      body: postData,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
   });
 
-  it(".uncheck() - uncheck a checkbox element", () => {
+  it("Get User Info", () => {
+    //This test sends a GET request to the Petstore API and checks
+    //if the response status is 200, indicating success.
+    cy.request("GET", "https://petstore.swagger.io/v2/user/ecem").then(
+      (response) => {
+        expect(response.status).to.eq(200);
+      }
+    );
+  });
 
+  it("Update User", () => {
+    //This test sends a PUT request to the Petstore API and checks
+    //if the response status is 200, indicating success.
+    const postData = {
+      id: 54321,
+      username: "ecem",
+      firstName: "ecem naz",
+      lastName: "görüşük",
+      email: "ecemnazgorusuk@gmail.com",
+      password: "123",
+      phone: "05053456765",
+      userStatus: 0,
+    };
+    cy.request({
+      method: "PUT",
+      url: "https://petstore.swagger.io/v2/user/ecem",
+      body: postData,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+
+  it("Delete User", () => {
+    //This test sends a DELETE request to the Petstore API and checks
+    //if the response status is 200, indicating success.
+    const headerBody = { accept: "application/json" };
+    cy.request({
+      method: "DELETE",
+      url: "https://petstore.swagger.io/v2/user/ecem",
+      Headers: headerBody,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
   });
 });
